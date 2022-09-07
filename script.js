4da09e5167c73b42d3a959042fc41214a41cc1af
Julian <williamsmjulian@gmail.com>
2022-09-07 10:40:18 -0400

    weather api

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-fetch('https://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=astro&output=json'),{
+var inputval = document.querySelector('#cityinput')
+var btn = document.querySelector('#add');
+var city = document.querySelector('#cityoutput')
+var descrip = document.querySelector('#description')
+var temp = document.querySelector('#temp')
+var wind = document.querySelector('#wind')
+
+apik = "3045dd712ffe6e702e3245525ac7fa38"
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputval.value+'&appid='+apik)  .then(res => res.json()),{
 method: 'POST',
 header: {
     'content-type':'application/json'
@@ -9,4 +18,4 @@ body: JSON.stringify({
 }
    .then(res => res.json())
    .then(data => console.log(data))
-   
+
